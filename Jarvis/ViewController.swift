bf4fca62d39e5b0b6cef46551e6efd23eb22ed38
Stephen Lindauer <stephen.lindauer@gmail.com>
2016-05-13 19:20:38 -0700

    Initial Commit

---
+++ Jarvis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jarvis
+//
+//  Created by Frank on 5/13/16.
+//  Copyright © 2016 Lindauer, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
